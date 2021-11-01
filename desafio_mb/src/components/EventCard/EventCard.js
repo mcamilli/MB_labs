18e4de37de80c814a6c809eeef99d49643dc70f4
mcamilli <marcelo.camilli91@gmail.com>
2021-11-01 09:12:05 -0300

    Adicionei a página lista de eventos

---
+++ desafio_mb/src/components/EventCard/EventCard.js
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import { BsFillCalendarFill, BsFillPinMapFill } from "react-icons/bs";
+
+const EventCard = (props) => {
+  return (
+    <Card sx={{ width: 450, margin: "20px" }}>
+      <CardActionArea>
+        <CardMedia
+          component= {"img"}
+          alt= {props.title}
+          height={"150px"}
+          image={props.image}
+          title={props.title}          
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div" align={"center"}>
+            {props.title}
+          </Typography>
+          <Typography variant="body2" color="text.secondary" align={"center"}>
+            <BsFillCalendarFill/> {props.data} 
+            <br/>           
+            <BsFillPinMapFill/> {props.local}
+          </Typography>
+          </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+}
+
+export default EventCard
